24380b8df2db5a3893b89d638e395f7b9cb7883f
Swaroop <swaroopkcs@gmail.com>
2022-03-14 22:03:08 -0400

    ModifiedScript

--- src/test/java/com/swaroop/learningSelenium/SeleniumTestTrial.java
+++ src/test/java/com/swaroop/learningSelenium/SeleniumTestTrial.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -23,16 +24,18 @@ public class SeleniumTestTrial {
 	@Test
 	public void sendAMessage() {
 		WebElement subjHandling = wd.findElement(By.cssSelector("#id_contact"));
+		WebElement emailInput = wd.findElement(By.cssSelector("#email"));
+		WebElement orderReference = wd.findElement(By.id("id_order"));
+		WebElement message = wd.findElement(By.cssSelector("#message"));
+		WebElement submitButton = wd.findElement(By.cssSelector("div [class ='submit']  span i[class='icon-chevron-right right']"));
 		Select select = new Select(subjHandling);
 		select.selectByValue("2");
-		WebElement emailInput = wd.findElement(By.cssSelector("#email"));
 		emailInput.sendKeys("458@email.com");
-		WebElement orderReference = wd.findElement(By.id("id_order"));
 		orderReference.sendKeys("456789");
-		WebElement message = wd.findElement(By.cssSelector("#message"));
 		message.sendKeys("Hello");
-		WebElement submitButton = wd.findElement(By.cssSelector("#submitMessage"));
-		submitButton.submit();
+		submitButton.click();
+		WebElement getSubmitMessage = wd.findElement(By.cssSelector("div [class ='alert alert-success']"));
+		Assert.assertEquals(getSubmitMessage.getText(), "Your message has been successfully sent to our team.", "The test does not match");
 	}
 	
 	@AfterMethod
